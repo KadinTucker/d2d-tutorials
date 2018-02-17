4b52e2d014c9088ca456461f3f36315d1af9ba7d
Kxoe <kxoethecat@gmail.com>
2018-02-17 15:39:50 -0700

    Added polygon filling and drawing and drawcolor example to shapes tutorial

--- 2-Shapes/source/MainScreen.d
+++ 2-Shapes/source/MainScreen.d
@@ -28,6 +28,11 @@ class MainScreen : Screen {
         this.container.renderer.drawLine(new iVector(500, 100), new iVector(550, 200), PredefinedColor.RED);
         this.container.renderer.drawRect(new iRectangle(100, 150, 50, 100), PredefinedColor.BLUE);
         this.container.renderer.fillRect(new iRectangle(300, 200, 100, 150), PredefinedColor.ORANGE);
+        this.container.renderer.drawColor = Color(150, 100, 25);
+        this.container.renderer.drawLine(new Segment!(int, 2)(new iVector(500, 300), new iVector(550, 350)));
+        this.container.renderer.drawPolygon!3(new iPolygon!3(new iVector(100, 400), new iVector(200, 400), new iVector(150, 485)));
+        this.container.renderer.drawColor = Color(100, 25, 25);
+        this.container.renderer.fillPolygon!3(new iPolygon!3(new iVector(200, 400), new iVector(300, 400), new iVector(250, 485)));
     }
 
     /**
